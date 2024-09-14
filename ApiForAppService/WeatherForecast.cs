75b7e0812a25bd476c80df9931853614602582eb
M4giic <66871586+M4giic@users.noreply.github.com>
2024-09-14 11:18:51 +0200

    add new field

--- ApiForAppService/WeatherForecast.cs
+++ ApiForAppService/WeatherForecast.cs
@@ -2,6 +2,7 @@ namespace ApiForAppService
 {
     public class WeatherForecast
     {
+        public string NowaInformacja => "Jakis tekst";
         public DateOnly Date { get; set; }
 
         public int TemperatureC { get; set; }
